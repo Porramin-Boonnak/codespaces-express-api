fba08626ea3860f2788f338141f96ea094a1695b
Porramin-Boonnak <121176464+Porramin-Boonnak@users.noreply.github.com>
2024-10-12 17:31:33 +0000

    connect_mysql

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { application } = require('express');
+const { v4: uuidv4} = require('uuid');
+var mysql = require('mysql');
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,32 +21,39 @@ app.use((req, res, next) => {
 });
 app.use(express.json());
 
-const products=[
-  {id:0,name:"Notebook Acer Swift",price:45900,img:"https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg"},
-  {id:1,name:"Notebook Asus Vivo",price:19900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg"},
-  {id:2,name:"Notebook Lenovo Ideapad",price:32900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg"},
-  {id:3,name:"Notebook MSI Prestige",price:54900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg"},
-  {id:4,name:"Notebook DELL XPS",price:99900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg"},
-  {id:5,name:"Notebook HP Envy",price:46900,img:"https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg"}];
+var con = mysql.createConnection({
+    host: "korawit.ddns.net",
+    user: "webapp",
+    password: "secret2024",
+    port : 3307,
+    database: "shop"
+});
+con.connect(function(err){
+    if (err) throw err;
+});
 
 // Get all products
 app.get('/api/products', function (req, res) {
-    if (products.length > 0) {
-        res.send(products);
-    } else {
-        res.status(400).send("No products found");
-    }
+    con.query("SELECT * FROM product", function (err,result,fields) {
+        if (err) throw res.status(400).send('Not found any products');
+        console.log(result);
+        res.send(result);
+    });
 });
 
 // Get a specific product by ID
 app.get('/api/products/:id', function (req, res) {
     const id = req.params.id;
-    const product = products.find(item => item.id === id);
-    if (product) {
-        res.send(product);
-    } else {
-        res.status(400).send('Product not found for id ' + id);
-    }
+    con.query("SELECT * FROM product where id="+id, function (err,result,fields){
+        if (err) throw err;
+        let product=result;
+        if(product.length>0){
+            res.send(product);
+        }
+        else{
+            res.status(400).send('Not found any products for'+id);
+        }
+    });
 });
 
 const port = 5000;
